611d0129964302e7a7f0822111b5ffbed232f83e
cccc <knsad15664761216@gmail.com>
2025-11-09 20:58:51 +0800

    利用nacos连接管理服务的项目

---
+++ spring_boot_nacos/src/test/java/com/chen/spring_boot_nacos/SpringBootNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.spring_boot_nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
